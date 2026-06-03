3c31a9723d01caf91a30312091b6e5f498dc7ea1
thomicz <tomassdvorakk@gmail.com>
2026-06-03 16:40:07 +0200

    update

--- app/learn/upload/page.tsx
+++ app/learn/upload/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"
 
 import React, { useMemo, useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
